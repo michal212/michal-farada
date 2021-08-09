4c0e06d14774800dcff17025e7a67dbcb6401c76
Yoni <yonatansamfisher@gmail.com>
2021-08-09 15:04:23 +0300

    updated

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 8080 ;
-const CONNECTION_URL= process.env.CONNECTION_URL ;
+const MONGO_URL= process.env.MONGO_URL ;
 const {getAllStudents,getStudentByName,deleteStudent} = require('../controllers/studentCtrl')
 const connectToDB = ()=>{
 return mongoose.connect(CONNECTION_URL,
--- server/server.js
+++ server/server.js
@@ -1,28 +1,25 @@
-const express = require('express')
-const app = express();
 const dotenv = require('dotenv').config();
-// const bodyparser = require('body-parser')
-const cors = require('cors')
-const PORT = process.env.PORT || 8000;
-const dbconnection = require('./db/index')
+const PORT = process.env.PORT || 8080 ;
+const CONNECTION_URL= process.env.CONNECTION_URL ;
+const studentRoute = require('./routes/studentRouter')
+const db = require('./db') // index יתפוס קבצי
+const express = require('express');
+const cors = require("cors");
+const app = express();
 
-dbconnection.on('error',()=>{
-    console.log('error')
-})
 
 app.use(express.json()); // JSON יכולת לקרוא ולהציג מידע מ
 app.use(express.urlencoded({extended:true})); // params יכולת לשלוף מידע מ
-app.use(cors()); // מוריד הרשאות גישה -  גורם לאקספרס לתת גישה לכולם להיכנס לסרבר / אתר
+app.use(cors());
 
-app.use('/',express.static('client'));
+db.on('error',()=> console.log("error"))
 
-// app.get('/',(req,res)=>{
-//     res.send('<h1>This is the main page</h1>')
-// })
-app.get('/',(req,res)=>{
-    res.send("<h1> This is main page </h1>")
+app.listen(PORT, ()=>{
+    console.log(`Server is up one port:${PORT}`);
 })
 
-app.listen(PORT,()=>{
-    console.log(`Logged to http://localhost:${PORT}`)
+app.use('/school',studentRoute)
+
+app.get('/', (req,res)=>{
+    res.send("Hello World")
 })
